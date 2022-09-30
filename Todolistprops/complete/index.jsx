8d48d115caa85ccf25773a0262b51800715e4425
ABHISHEK NARVARIYA <112860937+Abhisheklodhi641@users.noreply.github.com>
2022-09-30 19:09:39 +0530

    Update index.jsx

--- Todolistprops/complete/index.jsx
+++ Todolistprops/complete/index.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 
 const Complet = (props) => {
+    
     const {data} = props;
-    console.log("props=>",props); 
+    
     return(
         <div>
             <h4>complite list</h4>
-         {data.map((list,index)=>{
-            return(
-                <div>
-                    {list}
-                </div>
-            )
-         })}
+                 {data.map((list,index)=>{
+                        return(
+                               <div>
+                                  {list}
+                               </div>
+                             )
+                  }
+             )
+         }
         </div>
     )
 }
